2a4f102fbebfd942d3a5aaffcf6b683df0c3fcac
uykamera653-jpg <uykamera653@gmail.com>
2025-09-26 21:36:16 +0500

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,7 @@
+// Ish turlari – worker va buyurtmachi uchun bitta ro‘yxat ishlatiladi
+window.CATEGORIES = [
+  "Qurilish yordamchisi","Yuk tashish","Yuvish-tozalash","Ta’mirlash (elektr)","Santexnika",
+  "Bo‘yoqchi","Payvandchi","Oshpaz","Kuryer","Bog‘bon","Eshik-deraza","Mebellar",
+  "Konditsioner","Sartarosh","Avto yuvish","Avto ustasi","Kompyuter ustasi","Telefon ustasi",
+  "Qorovul","Boshqa"
+];
